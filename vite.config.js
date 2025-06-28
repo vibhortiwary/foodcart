faaed783bc75d897ee1bf2189b2efc099aa5f71d
vibhortiwary <163341245+vibhortiwary@users.noreply.github.com>
2025-06-28 19:07:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // This makes all assets use relative paths
 })
